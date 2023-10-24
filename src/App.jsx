604131828d8069b52d690b15e3c1f158f7bcaa16
Luke McCarthy <astartofanewlight@gmail.com>
2023-10-24 04:49:09 -0500

    lab 6 working

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,66 @@
 import React from "react";
 import "./App.css";
-// Step 3
 import axios from "axios";
-
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 console.log(API_KEY);
-class App extends React.Component {
 
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: false,
-      errorMessage: "",
+      cityName: "",
+      cityInfo: null,
     };
   }
 
-  // Step 2
   handleCityInput = (event) => {
     this.setState({
       cityName: event.target.value,
     });
   };
 
-    // Step 4 
-    // let location = await axios.get(
+  handleCityFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log("city name?", this.state.cityName);
+    console.log(API_KEY);
 
+    let URL = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.cityName}&format=json`;
 
-    // );
+    let cityInfo = await axios.get(URL);
 
-  // Step 5
-  // this.setState({
-  //   location: location.data.results,
-  //   error: false,
-  // });
+    console.log("city info: ", cityInfo.data[0]);
 
+    this.setState({
+      cityInfo: cityInfo.data[0],
+      error: false,
+    });
+  };
 
-render() {
-  return (<>
-    <h1>City Explorer</h1>
-    {/* Step 1 */}
-    <form onSubmit={this.handleSubmit}>
-      <button type="submit">Explore!</button>
+  render() {
 
-    </form>
+    return (
+      <>
+        <h1>World Explore</h1>
+        <form onSubmit={this.handleCityFormSubmit}>
+          <label>
+            Pick a City:
+            <input type="text" onChange={this.handleCityInput} />
+          </label>
+          <button type="submit"> Get City Data</button>
+        </form>
+        {this.state.cityInfo && (
+          <>
+            <p>{this.state.cityInfo.display_name}</p>
+            <p>{this.state.cityInfo.lat}</p>
+            <p>{this.state.cityInfo.lon}</p>
+          </>
+        )}
 
-
-
-  </>
-  );
-}
+      </>
+    );
+  }
 }
 
 export default App;
